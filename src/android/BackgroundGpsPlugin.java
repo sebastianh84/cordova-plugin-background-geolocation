e7ae21300b1d42db64b144d02bf9c6f74cea62dc
androidgecko <contact@androidgecko.com>
2014-12-31 17:23:07 +0100

    Resolve conflict cherry-picking from AndroidGecko

--- src/android/BackgroundGpsPlugin.java
+++ src/android/BackgroundGpsPlugin.java
@@ -51,12 +51,16 @@ public class BackgroundGpsPlugin extends CordovaPlugin {
         Boolean result = false;
         updateServiceIntent = new Intent(activity, LocationUpdateService.class);
 
+        this.callback = callbackContext;
+        
         if (ACTION_START.equalsIgnoreCase(action) && !isEnabled) {
             result = true;
             if (params == null || headers == null || url == null) {
                 callbackContext.error("Call configure before calling start");
             } else {
                 // callbackContext.success();
+
+
                 updateServiceIntent.putExtra("url", url);
                 updateServiceIntent.putExtra("params", params);
                 updateServiceIntent.putExtra("headers", headers);
@@ -77,7 +81,8 @@ public class BackgroundGpsPlugin extends CordovaPlugin {
             isEnabled = false;
             result = true;
             activity.stopService(updateServiceIntent);
-            // callback.success();
+            this.callback = null;
+            
         } else if (ACTION_CONFIGURE.equalsIgnoreCase(action)) {
             result = true;
             try {
@@ -101,7 +106,7 @@ public class BackgroundGpsPlugin extends CordovaPlugin {
         } else if (ACTION_SET_CONFIG.equalsIgnoreCase(action)) {
             result = true;
             // TODO reconfigure Service
-            // callback.success();
+            //callback.success();
         }
 
         return result;
